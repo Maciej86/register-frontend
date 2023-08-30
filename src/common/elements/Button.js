19ecfecbb1cf3a1c80224f51a5f39b16c4021f8d
Maciej Rościszewski <maciej.rosciszewski@progrupa.com>
2023-08-30 10:19:58 +0200

    Changed elements

--- src/common/elements/Button.js
+++ src/common/elements/Button.js
@@ -1,11 +1,8 @@
-import { LuSave } from "react-icons/lu";
-import { ImCancelCircle } from "react-icons/im";
 import { BoxButton } from "./styled";
-
-export const Button = ({ text, type = true, action = null }) => {
+export const Button = ({ text, icon = "", type = true, action = null }) => {
   return (
     <BoxButton type="button" $type={type} onClick={action}>
-      {type ? <LuSave size={"15px"} /> : <ImCancelCircle size={"15px"} />}
+      {icon}
       {text}
     </BoxButton>
   );
--- src/common/elements/InputText.js
+++ src/common/elements/InputText.js
@@ -1,25 +1,32 @@
+import { forwardRef } from "react";
 import { BoxInput, Input, Label } from "./styled";
 
-export const InputText = ({
-  id,
-  label,
-  type = "text",
-  maxlength = 20,
-  placeholder,
-  value = "",
-  disabled = "",
-}) => {
-  return (
-    <BoxInput>
-      <Label htmlFor={id}>{label}</Label>
-      <Input
-        id={id}
-        type={type}
-        maxLength={maxlength}
-        placeholder={placeholder}
-        defaultValue={value}
-        disabled={disabled}
-      />
-    </BoxInput>
-  );
-};
+export const InputText = forwardRef(
+  (
+    {
+      id,
+      label,
+      type = "text",
+      maxlength = 20,
+      placeholder,
+      value = "",
+      disabled = "",
+    },
+    ref
+  ) => {
+    return (
+      <BoxInput>
+        <Label htmlFor={id}>{label}</Label>
+        <Input
+          id={id}
+          type={type}
+          maxLength={maxlength}
+          placeholder={placeholder}
+          ref={ref}
+          defaultValue={value}
+          disabled={disabled}
+        />
+      </BoxInput>
+    );
+  }
+);
