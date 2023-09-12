b16da758220019e6ad030d0e6cecc6d8c208e359
Maciej Rościszewski <maciej.rosciszewski@progrupa.com>
2023-09-12 11:36:59 +0200

    Created new features  - Organization

--- src/features/Organization/index.js
+++ src/features/Organization/index.js
@@ -1,1 +1,13 @@
-export const Organization = () => <h1>Organizacje</h1>;
+import { Tile } from "../../common/Tile";
+
+export const Organization = () => {
+  return (
+    <div>
+      <Tile
+        title="Dodaj nową organizację"
+        subTitle="Stwórz nową organizację i przypisz do nich użytkowników."
+        body="Tutaj formularz"
+      />
+    </div>
+  );
+};
