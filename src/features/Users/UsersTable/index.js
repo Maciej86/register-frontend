5d068e2346c960848cbd4ab7e54d856b845345ca
Maciej Rościszewski <maciej.rosciszewski@progrupa.com>
2023-11-07 13:25:20 +0100

    Created new featuer

---
+++ src/features/Users/UsersTable/index.js
@@ -0,0 +1,12 @@
+import { TileTwoHalfAndContenet } from "../../../common/TileTwoHalfAndContent";
+
+export const UsersTable = () => {
+  return (
+    <TileTwoHalfAndContenet
+      title="Użytkownicy"
+      subTitle="Zarządzaj użytkownikami"
+      rightSite="Tutaj button dodaj uzytkownika"
+      content="Tutaj tabelka z uzytkownikami"
+    />
+  );
+};
--- src/features/Users/index.js
+++
@@ -1,1 +0,0 @@
-export const Users = () => <h1>Użytkownicy</h1>;
