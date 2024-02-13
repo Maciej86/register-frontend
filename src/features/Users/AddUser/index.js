93250068d917f77dca5434c4455f4cba827d6cb0
Maciej Rościszewski <maciej.rosciszewski@progrupa.com>
2024-02-13 14:58:33 +0100

    Add error connection server

--- src/features/Users/AddUser/index.js
+++ src/features/Users/AddUser/index.js
@@ -1,8 +1,11 @@
 import { Tile } from "../../../common/Tile";
+import { useErrorConnectServer } from "../../../core/hooks/useErrorConnectServer";
+import { selectErrorServerUser } from "../../../store/User/sliceUser";
 import { FormAddUser } from "./view/FormAddUser";
 
 export const AddUser = () => {
   const { formAddUser } = FormAddUser();
+  useErrorConnectServer(selectErrorServerUser, "storeUser");
 
   return (
     <Tile
