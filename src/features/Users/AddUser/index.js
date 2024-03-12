b68bc56cdbbda40feb38fffff15c635b95acce99
Maciej Rościszewski <maciej.rosciszewski@progrupa.com>
2024-03-12 15:01:54 +0100

    Add button

--- src/features/Users/AddUser/index.js
+++ src/features/Users/AddUser/index.js
@@ -1,17 +1,29 @@
+import { PiUsers } from "react-icons/pi";
 import { Tile } from "../../../common/Tile";
-import { USERSETTINGS } from "../../../core/InfoText";
+import { LinkButton } from "../../../common/styledLinkButton";
+import { NAVIGATION, USERSETTINGS } from "../../../core/InfoText";
 import { useErrorConnectServer } from "../../../core/hooks/useErrorConnectServer";
 import { selectErrorServerUser } from "../../../store/User/sliceUser";
+import { ButtonArea } from "../TableUsers/styled";
 import { FormAddUser } from "./view/FormAddUser";
 
 export const AddUser = () => {
   const { formAddUser } = FormAddUser();
   useErrorConnectServer(selectErrorServerUser, "storeUser");
 
+  const ButtonAllUsers = (
+    <ButtonArea>
+      <LinkButton to={`/${NAVIGATION.NAV_LINK_USERS}`}>
+        <PiUsers /> {USERSETTINGS.COM_SUBTITLE_TABLE_USERS}
+      </LinkButton>
+    </ButtonArea>
+  );
+
   return (
     <Tile
       title={USERSETTINGS.CREATE_ACCOUNT_TITLE}
       subTitle={USERSETTINGS.CREATE_ACCOUNT_SUBTITLE}
+      rightSide={ButtonAllUsers}
       content={formAddUser}
     />
   );
